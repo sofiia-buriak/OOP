4d9f2945fcc6f2d08fde0fb731e3b75c51f87b62
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-10-29 21:48:08 +0200

    У методі updateQuality була виправлена логіка оновлення якості для товарів типу Backstage passes, щоб уникнути надмірного збільшення якості. У методі processExpired була виправлена логіка для товарів, які прострочені, щоб уникнути неправильного зменшення якості для Sulfuras. Доданий метод decreaseQuality для зменшення якості товару, щоб зменшити дублювання коду та зробити його більш читабельним.

--- Лаба 3/Java/src/main/java/com/gildedrose/GildedRose.java
+++ Лаба 3/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -35,21 +35,19 @@ class GildedRose {
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            increaseQuality(item);
 
-                if (item.sellIn < 11) {
-                    increaseQuality(item);
-                }
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
 
-                if (item.sellIn < 6) {
-                    increaseQuality(item);
-                }
+            if (item.sellIn < 6) {
+                increaseQuality(item);
             }
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return; // Додано крапку з комою
-        } else if (item.quality > 0) {
-            item.quality = item.quality - 1;
+        } else {
+            decreaseQuality(item);
         }
     }
 
@@ -63,16 +61,19 @@ class GildedRose {
     private void processExpired(Item item) {
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
         } else {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                item.quality = 0;
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
             }
+            decreaseQuality(item);
+        }
+    }
+
+    private static void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
