c211aee5fc7def734c7e10c03a8d6d694058689b
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-10-31 19:42:10 +0200

    Додано клас ItemName, який представляє назву товару та містить методи для отримання назви та порівняння з іншими строками та інкапсуляція логіки.

--- Лаба 3/Java/src/main/java/com/gildedrose/Item.java
+++ Лаба 3/Java/src/main/java/com/gildedrose/Item.java
@@ -1,19 +1,6 @@
 package com.gildedrose;
 
 public class Item {
-
-    public String name;
-
-    public ItemProperties properties;
-
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+    private final ItemName name;
+    private final ItemProperties properties;
 }
