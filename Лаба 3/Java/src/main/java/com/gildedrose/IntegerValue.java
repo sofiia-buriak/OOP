e6d4eedda5cdd0d4b454d588d03121bec4feeeda
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-10-31 21:58:51 +0200

    Додано клас IntegerValue, який представляє ціле число та містить методи для отримання значення.

---
+++ Лаба 3/Java/src/main/java/com/gildedrose/IntegerValue.java
@@ -0,0 +1,13 @@
+package com.gildedrose;
+
+public class IntegerValue {
+    private final int value;
+
+    public IntegerValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
