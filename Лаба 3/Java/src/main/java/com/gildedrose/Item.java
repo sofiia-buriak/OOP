42cf4d99caf234bdab0546ad1db6540d461180ae
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-10-31 19:51:26 +0200

    Клас Item, який тепер використовує об'єкти ItemName та ItemProperties дляпредставлення назви та властивостей товару.Методи для отримання назви та властивостей товару.Перевизначений метод toString для зручного виведення інформації про товар. Це робиться, бо дозволяє інкапсулювати логіку, пов'язану з цими властивостями, що зменшує залежність між класами та спрощує підтримку коду.

--- Лаба 3/Java/src/main/java/com/gildedrose/Item.java
+++ Лаба 3/Java/src/main/java/com/gildedrose/Item.java
@@ -3,4 +3,24 @@ package com.gildedrose;
 public class Item {
     private final ItemName name;
     private final ItemProperties properties;
+
+    public Item(ItemName name, ItemProperties properties) {
+        this.name = name;
+        this.properties = properties;
+    }
+
+    public ItemName getName() {
+        return name;
+    }
+
+    public ItemProperties getProperties() {
+        return properties;
+    }
+
+    @Override
+    public String toString() {
+        return name.getValue() + ", " +
+                properties.getSellIn().getValue() + ", " +
+                properties.getQuality().getValue();
+    }
 }
