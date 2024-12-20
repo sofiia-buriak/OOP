fa5c1fdaaa4e1a5fe63d1f5a15e0c467bbde7261
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-12-20 21:44:58 +0200

    створення бажаного коду

---
+++ Лаба 5/demo/src/main/java/com/example/MediaLibraryDemo.java
@@ -0,0 +1,13 @@
+public class MediaLibraryDemo {
+    public static void main(String[] args) {
+        MediaLibrary library = new MediaLibrary();
+
+        library.addMedia(new MediaFile("song.mp3", MediaType.AUDIO, 1024, "2024-12-01"));
+        library.addMedia(new MediaFile("movie.mp4", MediaType.VIDEO, 20480, "2024-12-02"));
+
+        Iterator<MediaFile> iterator = library.iterator(file -> file.getType() == MediaType.AUDIO);
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().getName());
+        }
+    }
+}
