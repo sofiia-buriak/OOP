f9a9b25e093ef6f85f70e0800d5193a7757273b7
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-12-22 20:32:34 +0200

    забула зберегти зміни тому ось MediaFile

--- untitled/src/test/java/model/MediaFileTest.java
+++ untitled/src/test/java/model/MediaFileTest.java
@@ -3,43 +3,38 @@ package model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.enums.MediaType;
+import org.example.model.AudioFile;
+import org.example.model.ImageFile;
 import org.example.model.MediaFile;
+import org.example.model.VideoFile;
 import org.junit.jupiter.api.Test;
 
-class MediaFileTest {
+public class MediaFileTest {
 
-    private static class TestMediaFile extends MediaFile {
-        public TestMediaFile(String name, int size, String date) {
-            super(name, size, date);
-        }
-
-        @Override
-        public MediaType getType() {
-            return MediaType.IMAGE; 
-        }
+    @Test
+    public void testAudioFile() {
+        MediaFile audioFile = new AudioFile("song.mp3", 1024, "2024-12-01");
+        assertEquals("song.mp3", audioFile.getName());
+        assertEquals(1024, audioFile.getSize());
+        assertEquals("2024-12-01", audioFile.getDate());
+        assertEquals(MediaType.AUDIO, audioFile.getType());
     }
 
     @Test
-    void testGetName() {
-        MediaFile mediaFile = new TestMediaFile("example", 100, "2023-10-01");
-        assertEquals("example", mediaFile.getName());
+    public void testImageFile() {
+        MediaFile imageFile = new ImageFile("picture.jpg", 512, "2024-12-03");
+        assertEquals("picture.jpg", imageFile.getName());
+        assertEquals(512, imageFile.getSize());
+        assertEquals("2024-12-03", imageFile.getDate());
+        assertEquals(MediaType.IMAGE, imageFile.getType());
     }
 
     @Test
-    void testGetSize() {
-        MediaFile mediaFile = new TestMediaFile("example", 100, "2023-10-01");
-        assertEquals(100, mediaFile.getSize());
-    }
-
-    @Test
-    void testGetDate() {
-        MediaFile mediaFile = new TestMediaFile("example", 100, "2023-10-01");
-        assertEquals("2023-10-01", mediaFile.getDate());
-    }
-
-    @Test
-    void testGetType() {
-        MediaFile mediaFile = new TestMediaFile("example", 100, "2023-10-01");
-        assertEquals(MediaType.IMAGE, mediaFile.getType());
+    public void testVideoFile() {
+        MediaFile videoFile = new VideoFile("movie.mp4", 20480, "2024-12-02");
+        assertEquals("movie.mp4", videoFile.getName());
+        assertEquals(20480, videoFile.getSize());
+        assertEquals("2024-12-02", videoFile.getDate());
+        assertEquals(MediaType.VIDEO, videoFile.getType());
     }
 }
