d76f23c4946eca7cf750a73b7dbe5372a7c53de0
Буряк Софія <146016814+sofiia-buriak@users.noreply.github.com>
2024-12-22 16:52:13 +0200

    створено реалізацію методів

--- untitled/src/main/java/org/example/MediaLibrary.java
+++ untitled/src/main/java/org/example/MediaLibrary.java
@@ -3,26 +3,32 @@ package org.example;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
 import java.util.Comparator;
 
-public class MediaLibrary {
+class MediaLibrary {
+    private final List<MediaFile> mediaFiles = new ArrayList<>();
+
     public void addMedia(MediaFile file) {
+        mediaFiles.add(file);
     }
 
     public Iterator<MediaFile> iterator(Predicate<MediaFile> filter) {
-        return null; 
+        return mediaFiles.stream().filter(filter).iterator();
     }
 
     public Iterator<MediaFile> sortedIterator(Comparator<MediaFile> comparator) {
-        return null;
+        List<MediaFile> sortedList = new ArrayList<>(mediaFiles);
+        sortedList.sort(comparator);
+        return sortedList.iterator();
     }
 
-
     public List<MediaFile> search(Predicate<MediaFile> filter) {
-        return null; 
+        return mediaFiles.stream().filter(filter).collect(Collectors.toList());
     }
 
     public void removeMedia(Predicate<MediaFile> filter) {
+        mediaFiles.removeIf(filter);
     }
 }
-
